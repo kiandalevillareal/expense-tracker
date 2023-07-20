b5fae93688ee0af884d4772c748af116ed62c329
kiandalevillareal <kiandalevillareal@gmail.com>
2023-07-20 16:21:36 +0800

    added: edit button for each input

--- index.js
+++ index.js
@@ -15,19 +15,28 @@ window.addEventListener('load', () => {
 			return;
 		}
 
-		// Task Input
 		const taskEl = document.createElement('div');
 		taskEl.classList.add('task');
 
 		const taskContent = document.createElement('div');
 		taskContent.classList.add('content');
 
+		// Task Input
 		const inputTaskEl = document.createElement('input');
 		inputTaskEl.classList.add('text');
 		inputTaskEl.type = 'text';
 		inputTaskEl.value = task;
 		inputTaskEl.setAttribute('readonly', 'readonly');
 		
+		const editTaskButton = document.createElement('button');
+		editTaskButton.classList.add('edit-task');
+		editTaskButton.innerHTML = '<i class="fas fa-edit"></i>';
+
+		const taskWrapper = document.createElement('div');
+		taskWrapper.classList.add('task-wrapper');
+		taskWrapper.append(inputTaskEl);
+		taskWrapper.append(editTaskButton);
+
 		// Amount Input
 		const inputAmountEl = document.createElement('input');
 		inputAmountEl.classList.add('number');
@@ -35,12 +44,17 @@ window.addEventListener('load', () => {
 		inputAmountEl.value = amount;
 		inputAmountEl.setAttribute('readonly', 'readonly');
 
+		const editAmountButton = document.createElement('button');
+		editAmountButton.classList.add('edit-amount');
+		editAmountButton.innerHTML = '<i class="fas fa-edit"></i>';
+
+		const amountWrapper = document.createElement('div');
+		amountWrapper.classList.add('amount-wrapper');
+		amountWrapper.append(inputAmountEl);
+		amountWrapper.append(editAmountButton);
+
 		const actions = document.createElement('div');
 		actions.classList.add('actions');
-
-		const editButton = document.createElement('button');
-		editButton.classList.add('edit');
-		editButton.innerHTML = '<i class="fas fa-edit"></i>';
 
 		const deleteButton = document.createElement('button');
 		deleteButton.classList.add('delete');
@@ -50,30 +64,42 @@ window.addEventListener('load', () => {
 		saveButton.classList.add('save');
 		saveButton.innerHTML = '<i class="fas fa-check-square"></i>';
 
-		taskContent.append(inputTaskEl);
-		taskContent.append(inputAmountEl);
+		taskContent.append(taskWrapper);
+		taskContent.append(amountWrapper);
 		taskEl.append(taskContent);
 		taskList.append(taskEl);
-		actions.append(editButton);
+		// actions.append(editButton);
 		actions.append(deleteButton);
 		taskEl.append(actions);
 
 		inputTask.value = "";
 		inputAmount.value = "";
 
-		editButton.addEventListener('click', () => {
-			if (editButton.innerHTML.toLowerCase().includes('edit')) {
+		editTaskButton.addEventListener('click', () => {
+			if (editTaskButton.innerHTML.toLowerCase().includes('edit')) {
 				inputTaskEl.removeAttribute('readonly');
 				inputTaskEl.focus();
-				editButton.innerHTML = '<i class="fas fa-check-square"></i>';
+				editTaskButton.innerHTML = '<i class="fas fa-check-square"></i>';
 			} else {
 				inputTaskEl.setAttribute('readonly', 'readonly');
-				editButton.innerHTML = '<i class="fas fa-edit"></i>';
+				editTaskButton.innerHTML = '<i class="fas fa-edit"></i>';
+			}
+		});
+
+		editAmountButton.addEventListener('click', () => {
+			if (editAmountButton.innerHTML.toLowerCase().includes('edit')) {
+				inputAmountEl.removeAttribute('readonly');
+				inputAmountEl.focus();
+				editAmountButton.innerHTML = '<i class="fas fa-check-square"></i>';
+			} else {
+				inputAmountEl.setAttribute('readonly', 'readonly');
+				editAmountButton.innerHTML = '<i class="fas fa-edit"></i>';
 			}
 		});
 
 		deleteButton.addEventListener('click', () =>{
 			taskEl.remove();
+			console.log('test');
 		});
 	})	
 });
